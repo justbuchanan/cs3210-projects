f3e7117d23c9e61fdec3fb839e84f7448896b638
Roi Atalla <roi@roiatalla.com>
2015-10-01 19:25:34 -0400

    Removed logging and useless variables.

--- log.c
+++ log.c
@@ -9,10 +9,6 @@ unsigned int start, end;
 
 struct mutex log_mutex;
 
-unsigned int syscall_count = 0;
-unsigned int log_max_len = 0;
-unsigned long long log_total_len = 0;
-
 void init_log(void) {
 	mutex_init(&log_mutex);
 	logs_buffer = kcalloc(LOG_BUFFER_CAPACITY, sizeof(char*), GFP_KERNEL);
@@ -52,35 +48,22 @@ ssize_t read_log(struct file* filp, char* buffer, size_t count,
 
 	mutex_unlock(&log_mutex);
 
-	printk(KERN_INFO "Read from the log.\n");
-
 	return total_copied;
 }
 
 void monitor_handler(const char* logline) {
-	static char printed = 0;
-
 	mutex_lock(&log_mutex);
 
 	unsigned int nextIdx = (end + 1) % LOG_BUFFER_CAPACITY;
 
 	if(nextIdx == start) {
 		kfree(logs_buffer[start]);
-
-		if(!printed) {
-			printk(KERN_INFO "STARTED TO FREE!\n");
-			printed = 1;
-		}
-		
 		start = (start + 1) % LOG_BUFFER_CAPACITY;
-	}
-	else {
-		printed = 0;
 	}
 
 	size_t len = strlen(logline) + 2;
 	logs_buffer[end] = kmalloc(len, GFP_KERNEL);
-	memcpy(logs_buffer[end], logline, len);
+	memcpy(logs_buffer[end], logline, len - 1);
 	logs_buffer[end][len - 2] = '\n';
 	logs_buffer[end][len - 1] = 0;
 	end = nextIdx;
