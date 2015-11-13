18da9b5f303a36a333212c80eb5bb019e5db51e7
Justin Buchanan <justbuchanan@gmail.com>
2015-11-13 15:51:57 -0500

    updated test program

--- deadlocks-project2/lock_kmod/test/main.c
+++ deadlocks-project2/lock_kmod/test/main.c
@@ -2,15 +2,21 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../../custom_syscall.h"
 
 
 int main(int argc, char** argv) {
-    int param = 1;
-    if (argc > 1) {
-        param = atoi(argv[1]);
+    if (argc == 1) {
+        fprintf(stderr, "Error, no syscall number provided\n");
+        return -1;
     }
-    syscall(CustomSyscallNumber, param);
+    int sysnum = atoi(argv[1]);
+    int param = 1;
+    if (argc > 2) {
+        param = atoi(argv[2]);
+    }
+    syscall(sysnum, param);
     return 0;
 }
