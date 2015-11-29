a022c49691ec9913b98bb3af8cb5d14c093471f4
Mohit <mohittandon@gatech.edu>
2015-11-29 05:24:10 -0500

    Updated android-fs

--- yourtunes-project3/fs/main.hpp
+++ yourtunes-project3/fs/main.hpp
@@ -5,6 +5,7 @@
 static std::string base_url = "mediafs.azurewebsites.net";
 static std::string metadata_url = base_url + "/api/metadata";
 static std::string song_url = base_url + "/api/song";
+static std::string download_url = "https://cs3210mediafs.blob.core.windows.net/media/";
 
 bool execute(const char* cmd);
 bool executeAndReloadMetadata(std::string);
